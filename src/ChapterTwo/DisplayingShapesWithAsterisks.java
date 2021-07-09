4220f0e2b79fb7aeaea6d177e3a36d3b517ba702
Isaac John <ijenubiak@gmail.com>
2021-07-09 02:49:02 +0100

    Chapter Two

---
+++ src/ChapterTwo/DisplayingShapesWithAsterisks.java
@@ -0,0 +1,17 @@
+package ChapterTwo;
+
+public class DisplayingShapesWithAsterisks {
+
+    public static void main(String[] args) {
+
+        System.out.println("*********\t\t***\t\t\t*\t\t  *");
+        System.out.println("*\t\t*\t  *\t\t*\t   ***\t\t*\t*");
+        System.out.println("*\t\t*\t *\t\t *\t  *****\t   *\t  *");
+        System.out.println("*\t\t*\t *\t\t *\t\t*\t *\t\t    *");
+        System.out.println("*\t\t*\t *\t\t *\t\t*   *\t\t\t *");
+        System.out.println("*\t\t*\t *\t\t *\t\t*\t *\t\t\t*");
+        System.out.println("*\t\t*\t *\t\t *\t\t*\t   *\t  *");
+        System.out.println("*\t\t*\t  *\t\t*\t    *\t\t*\t*");
+        System.out.println("*********\t\t***\t\t\t*\t\t  *");
+    }
+}
