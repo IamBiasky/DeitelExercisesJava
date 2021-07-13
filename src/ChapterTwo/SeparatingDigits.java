3ec6c9bec24f876765812fbeacd61f30ded933e6
Isaac John <ijenubiak@gmail.com>
2021-07-13 10:01:08 +0100

    Chapter Two

--- src/ChapterTwo/SeparatingDigits.java
+++ src/ChapterTwo/SeparatingDigits.java
@@ -8,25 +8,36 @@ public class SeparatingDigits {
 
         Scanner input = new Scanner(System.in);
 
-        int number1, number2, number3, number4, number5;
-
         System.out.println("Enter a number: ");
-        number1 = input.nextInt();
+        int number = input.nextInt();
 
-        if (9999 < number1 && number1 < 100000)
+        int firstDigit = number/10000;
 
-            number5 = number1 % 10;
-            number1 = number1/10;
+        int secondNumber = number % 10000;
+        int secondDigit = secondNumber / 1000;
 
-            number4 = number1 % 10;
-            number1 = number1/10;
+        int thirdNumber = number % 1000;
+        int thirdDigit = thirdNumber / 100;
 
-            number3 = number1 % 10;
-            number1 = number1/10;
+        int fourthNumber = thirdNumber % 100;
+        int fourthDigit = fourthNumber / 10;
 
-            number2 = number1 % 10;
-            number1 = number1/10;
+        int fifthDigit = fourthNumber % 10;
 
+
+        System.out.println(firstDigit + "  " + secondDigit + "  " + thirdDigit + "  " + fourthDigit + "  " + fifthDigit);
+
+//            number5 = number4 % 10;
+//
+//            number4 = number1 % 10;
+//            number1 = number1/10;
+//
+//            number3 = number1 % 10;
+//            number1 = number1/10;
+//
+//            number2 = number1 % 10;
+//            number1 = number1/10;
+//
 //        System.out.printf("%d\t%d\t%d\t%d\t%d", number1, number2, number3, number4, number5);
     }
 }
